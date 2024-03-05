6030e6340aded7f4c6f21e3acbc65e055fe8b0fc
DharmatejaKathi <kathidharmateja@gmail.com>
2024-03-05 07:45:54 +0000

    IPL Dash Board

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,10 @@
+// Write your code here
+import './index.css'
+
+const NotFound = () => (
+  <div className="Not-c">
+    <h1 className="not">Page Not Found!!</h1>
+  </div>
+)
+
+export default NotFound
